0251c35a178c451e90fcce2a074a0412fdfa97e1
thirteenfour <nikko.gammad@gmail.com>
2025-02-13 15:19:04 +0800

    basic camera file

---
+++ python-gui-camera-test.py
@@ -0,0 +1,14 @@
+import cv2
+# import time
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, image = cam.read()
+    cv2.imshow('Imagetest',image)
+    k = cv2.waitKey(1)
+    if k != -1:
+        break
+cv2.imwrite('/home/sprouttech/Desktop/testimage.jpg', image)
+cam.release()
+cv2.destroyAllWindows()
