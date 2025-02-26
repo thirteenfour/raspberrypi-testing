515c57350a90b5ed4aa51b9e77245933363585e3
thirteenfour <nikko.gammad@gmail.com>
2025-02-26 19:36:39 +0800

    additions to camera test

--- python-gui-camera-test.py
+++ python-gui-camera-test.py
@@ -6,6 +6,11 @@ cam = cv2.VideoCapture(0)
 while True:
     ret, image = cam.read()
     cv2.imshow('Imagetest',image)
+    
+    # convert to base64
+    # convert to multipart data
+    # submit to api
+    # flask?
     k = cv2.waitKey(1)
     if k != -1:
         break
